9796e0b0a0319747fe98245d17a16010828ad882
canpham-vn <canpham0610@gmail.com>
2024-04-12 22:05:01 +0700

    Config mongodb and mongoose

--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,11 @@
-import express, { Express, Request, Response } from "express";
+import express, { Express } from "express";
 import dotenv from "dotenv";
 import http from "http";
 import bodyParser from "body-parser";
 import cookieParser from "cookie-parser";
 import compression from "compression";
 import cors from "cors";
+import mongoose from "mongoose";
 
 dotenv.config();
 
@@ -27,3 +28,9 @@ const port = process.env.PORT || 3000;
 server.listen(port, () => {
   console.log("Server listening on port " + port);
 });
+
+const MONGO_URL = `mongodb+srv://${process.env.MONGO_USERNAME}:${process.env.MONGO_PASSWORD}@cluster0.yovabgv.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
+
+mongoose.Promise = Promise;
+mongoose.connect(MONGO_URL);
+mongoose.connection.on("error", (error: Error) => console.log(error));
