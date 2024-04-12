b969760a98dada7283a1f10adf5063b2038a5685
canpham-vn <canpham0610@gmail.com>
2024-04-12 21:41:40 +0700

    Install necessary packages

--- src/index.ts
+++ src/index.ts
@@ -1,15 +1,29 @@
 import express, { Express, Request, Response } from "express";
 import dotenv from "dotenv";
+import http from "http";
+import bodyParser from "body-parser";
+import cookieParser from "cookie-parser";
+import compression from "compression";
+import cors from "cors";
 
 dotenv.config();
 
 const app: Express = express();
+
+app.use(
+  cors({
+    credentials: true,
+  })
+);
+
+app.use(compression());
+app.use(cookieParser());
+app.use(bodyParser.json());
+
+const server = http.createServer(app);
+
 const port = process.env.PORT || 3000;
 
-app.get("/", (req: Request, res: Response) => {
-  res.send("Express + TypeScript Server");
-});
-
-app.listen(port, () => {
-  console.log(`[server]: Server is running at http://localhost:${port}`);
+server.listen(port, () => {
+  console.log("Server listening on port " + port);
 });
