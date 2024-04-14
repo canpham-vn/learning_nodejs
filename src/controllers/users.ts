e49650167f51f023a2a2360707fe2a7c88783079
canpham-vn <canpham0610@gmail.com>
2024-04-14 10:41:07 +0700

    Handle update user

--- src/controllers/users.ts
+++ src/controllers/users.ts
@@ -1,6 +1,6 @@
 import express from "express";
 
-import { deleteUserById, getUsers } from "../db/users";
+import { deleteUserById, getUserById, getUsers } from "../db/users";
 
 export const getAllUsers = async (
   req: express.Request,
@@ -25,6 +25,34 @@ export const deleteUser = async (
     const deleteUser = await deleteUserById(id);
 
     return res.status(200).json(deleteUser);
+  } catch (error) {
+    console.log(error);
+    return res.status(400);
+  }
+};
+
+export const updateUser = async (
+  req: express.Request,
+  res: express.Response
+) => {
+  try {
+    const { id } = req.params;
+    const { username } = req.body;
+
+    if (!username) {
+      return res.sendStatus(403).json({ msg: "username is required!" });
+    }
+
+    const user = await getUserById(id);
+
+    if (!user) {
+      return res.sendStatus(403).json({ msg: "user is not found!" });
+    }
+
+    user.username = username;
+    await user.save();
+
+    return res.status(200).json(user);
   } catch (error) {
     console.log(error);
     return res.status(400);
